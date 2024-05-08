dd1dbeb823efc9e0fc55d49e6ab8fc7229f705c2
Blakiemon <theblakiemon@gmail.com>
2024-05-08 03:10:29 -0700

    attacker defender button functionality

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,11 +1,62 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react"
+import { OperatorType } from "../data/operatorInfo";
 
-function BigButton() {
+function BigButton({ children, onClick, isActive, className }: { children: React.ReactNode; onClick: () => void; isActive: boolean; className?: string }) {
     return (
-        <Button>
-            Click me!
-        </Button>
-    );
+        <Box
+        as={'button'}
+        onClick={onClick}
+        bgColor={'#2499ff'}
+        color={'#24262a'}
+        boxSizing={'border-box'}
+        cursor={'pointer'}
+        fontFamily={'scoutcond'}
+        fontWeight={'400'}
+        fontSize={'24px'}
+        border={'0'}
+        padding={'10px'}
+        margin={'10px'}
+        display={"inline-flex"}
+        justifyContent={'center'}
+        position={"relative"}
+        borderBottom='5px solid #2499ff'
+        borderColor={isActive ? '#00ffe5' : ""}
+        _hover={{
+            transition: 'background .15s ease-in',
+            borderColor: '#00ffe5',
+            _before: {
+                opacity: "1"
+            }
+            
+        }}
+        _before={{
+            content: '""',
+            position: "absolute",
+            opacity: isActive ? "1" : "0",
+            top: "0",
+            left: "0",
+            width: '100%',
+            height: '100%',
+            background: 'linear-gradient(0deg, rgba(0, 255, 230, .5), rgba(0, 255, 230, 0) 50%)',
+            transition: 'opacity .15s ease-in'
+        }}
+        className={`big-button ${className == undefined ? "" : className}`}
+    >
+        <Text
+        as={"span"}
+        transition="transform .15s ease-in"
+        className="big-button-span"
+        display= "flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        height="100%"
+        transform={isActive ? "scale(1.1)" : "scale(1)"}
+        >{children}</Text>
+        
+    </Box>
+    )
 }
 
-export default BigButton;
+export default BigButton
--- src/pages/OperatorSelect.tsx
+++ src/pages/OperatorSelect.tsx
@@ -3,23 +3,28 @@ import { OperatorData, OperatorSpecialty, OperatorType } from "../data/operatorI
 import ButtonSelectGroup from "../components/ButtonSelectGroup";
 import BigButton from "../components/BigButton";
 import { MapData } from "../data/mapInfo";
+import { useState } from 'react';
 
 function OperatorSelect({ mapData }: { mapData: MapData }) {
-    const onTypeSelect = (option: OperatorType | null) => {
-        console.log(option);
+    const onTypeSelect = (option: OperatorType) => {
+        setSelectedOperatorType(option)
     };
 
     const onSpecialtySelect = (option: OperatorSpecialty | null) => {
         console.log(option);
     };
 
+    const [selectedOperatorType, setSelectedOperatorType] = useState<OperatorType>("attacker");
+
     console.log(mapData)
 
     return (
-        <Stack direction={'column'}>
+        <Stack direction={'column'} spacing={25}>
+            <Text fontSize={'80px'} fontFamily={'scoutcond'} textTransform={'uppercase'} lineHeight={'67px'} mt={50} color={'#fff'} textAlign={'center'}>SELECT AN OPERATOR</Text>
+            <Stack direction={'column'}>
             <Stack direction={'row'} spacing={5} justify={'center'} marginY={10}>
-                <BigButton></BigButton>
-                <BigButton></BigButton>
+                <BigButton onClick={() => onTypeSelect("attacker")} className="attacker-big-button" isActive={selectedOperatorType == "attacker"}>ATTACKER</BigButton>
+                <BigButton onClick={() => onTypeSelect("defender")} className="defender-big-button" isActive={selectedOperatorType == "defender"}>DEFENDER</BigButton>
             </Stack>
             <Stack direction={'column'} spacing={0}>
                 <Text fontSize={'24px'} fontFamily={'scoutcond'} textTransform={'uppercase'} color={'#fff'} textAlign={'center'}>
@@ -28,6 +33,7 @@ function OperatorSelect({ mapData }: { mapData: MapData }) {
                 <ButtonSelectGroup<OperatorSpecialty> onSelect={onSpecialtySelect} options={['ANTI-ENTRY', 'ANTI-GADGET', 'BREACH', 'CROWD CONTROL', 'FRONT LINE', 'INTEL', 'MAP CONTROL', 'SUPPORT', 'TRAPPER']} />
             </Stack>
             GRID HERE
+        </Stack>
         </Stack>
     );
 }
