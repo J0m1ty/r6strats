a01becd380314c438cb2b022af7ae336a41b7d0d
Blakiemon <theblakiemon@gmail.com>
2024-05-07 18:12:52 -0700

    defender op info

--- src/data/operatorInfo.ts
+++ src/data/operatorInfo.ts
@@ -233,7 +233,187 @@ export const operatorList: OperatorData[] = [
     {
         name: 'thorn',
         image: '/operators/badges/Thorn.png',
-        type: 'attacker',
-        specialties: ['ANTI-ENTRY', 'SUPPORT']
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', 'TRAPPER']
+    },
+    {
+        name: 'thunderbird',
+        image: '/operators/badges/Thunderbird.png',
+        type: 'defender',
+        specialties: ['SUPPORT']
+    },
+    {
+        name: 'aruni',
+        image: '/operators/badges/Aruni.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', 'ANTI-GADGET']
+    },
+    {
+        name: 'melusi',
+        image: '/operators/badges/Melusi.png',
+        type: 'defender',
+        specialties: ['INTEL', 'CROWD CONTROL']
+    },
+    {
+        name: 'oryx',
+        image: '/operators/badges/Oryx.png',
+        type: 'defender',
+        specialties: ['SUPPORT']
+    },
+    {
+        name: 'wamai',
+        image: '/operators/badges/Wamai.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "TRAPPER"]
+    },
+    {
+        name: 'goyo',
+        image: '/operators/badges/Goyo.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "TRAPPER"]
+    },
+    {
+        name: 'warden',
+        image: '/operators/badges/Warden.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "INTEL"]
+    },
+    {
+        name: 'kaid',
+        image: '/operators/badges/Kaid.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "ANTI-GADGET"]
+    },
+    {
+        name: 'clash',
+        image: '/operators/badges/Clash.png',
+        type: 'defender',
+        specialties: ['INTEL', "CROWD CONTROL"]
+    },
+    {
+        name: 'maestro',
+        image: '/operators/badges/Maestro.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "INTEL"]
+    },
+    {
+        name: 'alibi',
+        image: '/operators/badges/Alibi.png',
+        type: 'defender',
+        specialties: ['INTEL', "TRAPPER"]
+    },
+    {
+        name: 'vigil',
+        image: '/operators/badges/Vigil.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "CROWD CONTROL"]
+    },
+    {
+        name: 'ela',
+        image: '/operators/badges/Ela.png',
+        type: 'defender',
+        specialties: ["CROWD CONTROL", "TRAPPER"]
+    },
+    {
+        name: 'lesion',
+        image: '/operators/badges/Lesion.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "TRAPPER"]
+    },
+    {
+        name: 'mira',
+        image: '/operators/badges/Mira.png',
+        type: 'defender',
+        specialties: ['INTEL', "SUPPORT"]
+    },
+    {
+        name: 'echo',
+        image: '/operators/badges/Echo.png',
+        type: 'defender',
+        specialties: ['INTEL', "CROWD CONTROL"]
+    },
+    {
+        name: 'caveira',
+        image: '/operators/badges/Caveira.png',
+        type: 'defender',
+        specialties: ['INTEL', "CROWD CONTROL"]
+    },
+    {
+        name: 'valkyrie',
+        image: '/operators/badges/Valkyrie.png',
+        type: 'defender',
+        specialties: ['INTEL', "SUPPORT"]
+    },
+    {
+        name: 'frost',
+        image: '/operators/badges/Frost.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "TRAPPER"]
+    },
+    {
+        name: 'valkyrie',
+        image: '/operators/badges/Valkyrie.png',
+        type: 'defender',
+        specialties: ['INTEL', "SUPPORT"]
+    },
+    {
+        name: 'mute',
+        image: '/operators/badges/Mute.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "CROWD CONTROL"]
+    },
+    {
+        name: 'smoke',
+        image: '/operators/badges/Smoke.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "TRAPPER"]
+    },
+    {
+        name: 'castle',
+        image: '/operators/badges/Castle.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "SUPPORT"]
+    },
+    {
+        name: 'pulse',
+        image: '/operators/badges/Pulse.png',
+        type: 'defender',
+        specialties: ['INTEL', "SUPPORT"]
+    },
+    {
+        name: 'doc',
+        image: '/operators/badges/Doc.png',
+        type: 'defender',
+        specialties: ["SUPPORT"]
+    },
+    {
+        name: 'rook',
+        image: '/operators/badges/Rook.png',
+        type: 'defender',
+        specialties: ["SUPPORT"]
+    },
+    {
+        name: 'jager',
+        image: '/operators/badges/Jager.png',
+        type: 'defender',
+        specialties: ['ANTI-GADGET', "SUPPORT"]
+    },
+    {
+        name: 'bandit',
+        image: '/operators/badges/Bandit.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "ANTI-GADGET"]
+    },
+    {
+        name: 'tachanka',
+        image: '/operators/badges/Tachanka.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "CROWD CONTROL"]
+    },
+    {
+        name: 'kapkan',
+        image: '/operators/badges/Kapkan.png',
+        type: 'defender',
+        specialties: ['ANTI-ENTRY', "TRAPPER"]
     },
 ];
