54bd1c28d18f019aff23b24722378c82f4749857
Blake Barentine <47361570+Blakiemon@users.noreply.github.com>
2024-06-28 19:55:30 -0700

    castle

---
+++ src/data/defenders/Castle.ts
@@ -0,0 +1,16 @@
+import { Specialty, Value } from "../Operator";
+import { Defender, DefenderGadget } from "../Defender";
+import { UMP45 } from "../weapons/submachine gun/UMP45";
+import { M1014 } from "../weapons/shotgun/M1014";
+import { Five_Seven_USG } from "../weapons/handgun/5.7 USG";
+import { SUPER_SHORTY } from "../weapons/shotgun/SUPER SHORTY";
+import { M45_MEUSOC } from "../weapons/handgun/M45 MEUSOC";
+
+export const Castle: Defender = new (class implements Defender {
+    specialties: Specialty[] = [ "anti-gadget", "crowd control" ];
+    health: Value = 2;
+    speed: Value = 2;
+    primary = [ UMP45, M1014 ];
+    secondary = [ Five_Seven_USG, SUPER_SHORTY, M45_MEUSOC ];
+    gadgets: DefenderGadget[] = [ "bulletproof camera", "proximity alarm" ];
+})();
---
+++ src/data/weapons/submachine gun/UMP45.ts
@@ -0,0 +1,22 @@
+import { Gun, GunDestruction, GunType } from "../../Gun";
+import { Mobility, WeaponCatagory } from "../../Weapon";
+
+export const UMP45: Gun = new (class implements Gun {
+    type: GunType = "primary";
+    catagory: WeaponCatagory = "submachine gun";
+    acog = true;
+
+    damage = 38;
+    fireRate = 600;
+    destruction: GunDestruction = "low";
+
+    magazine = 25;
+    capacity = 176;
+    reloadSpeed = 2.9;
+    
+    controlDifficulty = 2;
+    adsTime = 0.41;
+    hipAccuracy = 75;
+
+    mobility: Mobility = "0%";
+})();
