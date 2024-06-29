99cf22a9f0a8692d1e23b5dc80b119648a13574a
Blake Barentine <47361570+Blakiemon@users.noreply.github.com>
2024-06-28 20:45:22 -0700

    tachanka

---
+++ src/data/defenders/Tachanka.ts
@@ -0,0 +1,16 @@
+import { Specialty, Value } from "../Operator";
+import { Defender, DefenderGadget } from "../Defender";
+import { DP27 } from "../weapons/submachine gun/DP27";
+import { NINEX19VSN } from "../weapons/submachine gun/9X19VSN";
+import { GSH_18 } from "../weapons/handgun/GSH-18";
+import { PMM } from "../weapons/handgun/PMM";
+import { Bearing_9 } from "../weapons/machine pistol/BEARING 9";
+
+export const Tachanka: Defender = new (class implements Defender {
+    specialties: Specialty[] = [ "anti-entry", "crowd control" ];
+    health: Value = 3;
+    speed: Value = 1;
+    primary = [ DP27, NINEX19VSN ];
+    secondary = [ GSH_18, PMM, Bearing_9 ];
+    gadgets: DefenderGadget[] = [ "barbed wire", "deployable shield", "proximity alarm" ];
+})();
---
+++ src/data/weapons/submachine gun/DP27.ts
@@ -0,0 +1,22 @@
+import { Gun, GunDestruction, GunType } from "../../Gun";
+import { Mobility, WeaponCatagory } from "../../Weapon";
+
+export const DP27: Gun = new (class implements Gun {
+    type: GunType = "primary";
+    catagory: WeaponCatagory = "submachine gun";
+    acog = false;
+
+    damage = 60;
+    fireRate = 550;
+    destruction: GunDestruction = "full";
+
+    magazine = 70;
+    capacity = 281;
+    reloadSpeed = 4.6;
+    
+    controlDifficulty = 1;
+    adsTime = 0.53;
+    hipAccuracy = 60;
+
+    mobility: Mobility = "-5%";
+})();
